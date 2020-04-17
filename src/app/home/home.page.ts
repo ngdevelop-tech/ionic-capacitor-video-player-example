004f1c0347aa2c8eea5fa696642d810266d7a58e
Ankit.P <ankit.p@ezdi.us>
2020-04-17 23:02:59 +0530

    Install and Setup Capacitor Video Player

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+import * as PluginsLibrary from 'capacitor-video-player';
+const { CapacitorVideoPlayer, Device } = Plugins;
+
+
+import { Component, AfterViewInit } from '@angular/core';
 import { DataService, Video } from '../services/data.service';
 
 @Component({
@@ -6,14 +11,28 @@ import { DataService, Video } from '../services/data.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
+  videoPlayer: any;
   videos: Video[];
-  
+
   constructor(private data: DataService) {
     this.videos = data.getVidoes();
   }
 
+  async ngAfterViewInit() {
+    const info = await Device.getInfo();
+    if (info.platform === "ios" || info.platform === "android") {
+      this.videoPlayer = CapacitorVideoPlayer;
+    } else {
+      this.videoPlayer = PluginsLibrary.CapacitorVideoPlayer
+    }
 
+  }
 
-  
+  async play(url: string) {
+    document.addEventListener('jeepCapVideoPlayerPlay', (e: CustomEvent) => { console.log('Event jeepCapVideoPlayerPlay ', e.detail) }, false);
+    document.addEventListener('jeepCapVideoPlayerPause', (e: CustomEvent) => { console.log('Event jeepCapVideoPlayerPause ', e.detail) }, false);
+    document.addEventListener('jeepCapVideoPlayerEnded', (e: CustomEvent) => { console.log('Event jeepCapVideoPlayerEnded ', e.detail) }, false);
+    const res: any = await this.videoPlayer.initPlayer({ mode: "fullscreen", url: url });
+  }
 }
